badff70309cbff36dcb6d74ea0de3bba615685a9
Quan Gao <quangao@Quans-MacBook-Pro.local>
2017-11-30 23:49:29 -0600

    html skeleton added

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+
+
+
+var dashes = ["-", "-"];
+
+// document.getElementById("currentWord").innerHTML = "hi";
+if(onkeyUp) {
+	document.getElementById("currentWord").innerHTML = `
+	<h2>${dashes}</h2>`;}
