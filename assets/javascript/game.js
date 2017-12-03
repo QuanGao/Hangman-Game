8a0b50a2363e2fc36707fb1517fd1b0b09710287
Quan Gao <quangao@Quans-MacBook-Pro.local>
2017-12-02 19:22:44 -0600

    built basic game mechanics

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,73 +1,68 @@
 
 // variables
-var words = ["hello", "you"];
-var blanks = ["-----", "---"];
+var words = ["first", "second", "third"];
+var blanks = ["-----", "------", "-----"];
 var gameStatus = 0;
-var currentWord = words[0];
-var currentBlank = blanks[0];
-var wordArray = currentWord.split("");
-var blankArray = currentBlank.split("");
-var displayArray = blankArray;
 var currentScore = 0;
 var lives = 5;
 var letters = [];
+var currentWord = words[-1];
+var currentBlank = blanks[-1];
+ 
 
-// functions
-
-function GetIndexes (array, value) {
-	indexes = [];
-	var arr = array;
-	for (i = 0; i < array.length; i ++) {
-		var index = array.indexOf(value);
-		arr[index] = "x1z";
-			if(index !== -1) {indexes.push(index);};
-	};
-	return(indexes);
-} 
-function ReplaceByIndexes (array, value, arrayID) {
-	for(i = 0, i < arrayID.lenght, i++) {
-		array[arryID[i]] = array
-	}
-}
-
-
-// when key is pressed, game start.
-// computer chose a word and display blanks (dashes) same length as the word 
-document.onkeyup = function(event) {		
-	console.log(gameStatus)
+document.onkeyup = function(event) {
+	var playerChoice = event.key;		
 	if (gameStatus === 0) {
+		select = Math.floor(Math.random()*words.length);
+		currentWord = words[select];
+		currentBlank = blanks[select];	
 		document.getElementById("display").innerHTML = `<p>${currentBlank}</p>`;
 		document.getElementById("score").innerHTML = `<p>${currentScore}</p>`;
-		document.getElementById("guessRemain").innerHTML = `<p>${lives}</p>`;
-		gameStatus = 1;
 		lives = 5;
-		letters = [];
-		console.log(gameStatus);
-	} else {
-		var playerChoice = event.key;
-		var indexOfPlayerChoice = wordArray.indexOf(playerChoice);
-			if (indexOfPlayerChoice > -1) {	
-				displayArray[indexOfPlayerChoice] = playerChoice;
-				var currentDisplay = displayArray.join("");
-				letters.push(playerChoice)
-				document.getElementById("display").innerHTML = `<p>${currentDisplay}</p>`;
-				document.getElementById("progress").innerHTML = `<p>${letters}</p>`;
-				var indexOfDash = currentDisplay.split("").indexOf("-")
-					if (indexOfDash == -1) {
-						currentScore = currentScore + 1;
-						document.getElementById("score").innerHTML = `<p>${currentScore}</p>`
-						gameStatus = 0;
-					}
- 			} else {
- 				if (lives > 0) {
- 				lives = lives - 1;
- 				console.log(lives);
- 				document.getElementById("guessRemain").innerHTML = `<p>${lives}</p>`;
- 				} else { 
- 					gameStatus = 0;}
- 			}
- 		}
+		document.getElementById("guessRemain").innerHTML = `<p>${lives}</p>`;
+		letters = [];	
+		document.getElementById("progress").innerHTML = `<p>${letters}</p>`;
+		gameStatus = 1;	
 	}
+	if(gameStatus === 1 && letters.indexOf(playerChoice) === -1) {
+		if(lives > 0){
+			var wordArr = currentWord.split("");
+			var blankArr = currentBlank.split("");
+			var count = 0;
+			for (i = 0; i < blankArr.length; i ++) {
+				if(wordArr[i] === playerChoice) {
+					blankArr[i] = playerChoice;
+					count++;
+					currentBlank = blankArr.join("");
+					document.getElementById("display").innerHTML = `<p>${currentBlank}</p>`
+				}
+			}
+			if(count > 0){
+				letters.push(playerChoice);
+				document.getElementById("progress").innerHTML = `<p>${letters}</p>`;
+				if(currentBlank.indexOf("-") === -1){
+					alert("YOU WIN YOU GENIUS!");
+					score = score + 1;
+
+				}
+			} else {
+				lives = lives -1;
+				document.getElementById("guessRemain").innerHTML = `<p>${lives}</p>`;
+			}
+		} else {
+			alert("you lose!");
+			gameStatus = 0
+		}
+
+	}
+}	
+	
+
+
+
+
+
+
 
 
 
