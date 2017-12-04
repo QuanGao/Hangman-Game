db8e1735127a881295a540f97eb3b1eb4ddb7c60
Quan Gao <quangao@Quans-MacBook-Pro.local>
2017-12-03 22:47:05 -0600

    OOP defined objects and add function to replace letters with dashes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,73 +1,85 @@
-// variables
+
+// define variables
 var words = ["bruce wayne", "joker", "barry allen", "clark kent", "diana prince", "tony stark", "thor", "frank castle",
 "deadpool", "matthew murdock", "logan","peter parker"];
 var icons = ["batman.svg","joker.svg","flash.svg","superman.svg","wonderwoman.svg","ironman.svg","thor.svg",
 "punisher.svg","deadpool.svg","daredevil.svg","wolverine.svg","spidy.svg"];
+
+// This function replace letters in a string into an array of dashes while keeping the space between 2 or more words
+var dashed = function(phrase) {
+		var dash = [];
+		for(i = 0; i < phrase.length; i++){
+			if(phrase[i] === " ") {
+				dash.push(" ");
+			} else {
+				dash.push("-");
+			};
+		};
+		return(dash);
+	};	
+
+// Create object prototype that can turn any selected word into object.
+function word(name, icon) {
+	this.name = name;
+	this.nameArr = name.split("");
+	this.length = name.length;
+	this.dashArr = dashed(name);
+	this.dashes = (this.dashArr).join("");
+	this.icon = `assets/images/${icon}`
+};
+
 var gameStatus = 0;
 var currentScore = 0;
-var lives = 10;
 var letters = [];
+var lives = 10;
+var currentBlank = [];
 var currentWord = "";
-var blank = [];
-var currentBlank = "";
-var displayIcon = ""; 
 
 document.onkeyup = function(event) {
 	var playerChoice = event.key;		
 	if (gameStatus === 0) {
 		select = Math.floor(Math.random()*words.length);
-		currentWord = words[select];
-		displayIcon = icons[select];
-		console.log(currentWord);
-		console.log(displayIcon);
-		blank = [];
-		currentBlank = ""; 
-		for(i = 0; i < currentWord.length; i++){
-			if(currentWord[i] === " ") {
-				blank.push(" ")
-			} else {blank.push("-")};		
-		};	
-		currentBlank = blank.join("");
-		console.log(currentBlank);
-		document.getElementById("display").innerHTML = `<p>${currentBlank}</p>`;
+		currentWord = new word(words[select],icons[select]);
+		currentBlank = currentWord.dashes();
+		document.getElementById("display").innerHTML = `<p>${currentBlank.join("")}</p>`;
 		document.getElementById("score").innerHTML = `<p>${currentScore}</p>`;
 		lives = 10;
 		document.getElementById("guessRemain").innerHTML = `<p>${lives}</p>`;
 		letters = [];	
 		document.getElementById("progress").innerHTML = `<p>${letters}</p>`;
 		gameStatus = 1;	
+
 	} else if(gameStatus === 1 && letters.indexOf(playerChoice) === -1) {
 		letters.push(playerChoice);
 		document.getElementById("progress").innerHTML = `<p>${letters}</p>`;
-		if(lives > 0){
-			var wordArr = currentWord.split("");
-			var blankArr = currentBlank.split("");
+		if(lives > 0) {
 			var count = 0;
-			for (i = 0; i < blankArr.length; i ++) {
-				if(wordArr[i] === playerChoice) {
-					blankArr[i] = playerChoice;
+			var currentArr = currentWord.nameArr;
+			console.log(currentArr);
+			for (i = 0; i < currentBlank.length; i ++) {
+				if(currentArr[i] === playerChoice) {
+					currentBlank[i] = playerChoice;
 					count++;
-					currentBlank = blankArr.join("");
-					document.getElementById("display").innerHTML = `<p>${currentBlank}</p>`
+					document.getElementById("display").innerHTML = `<p>${currentBlank.join("")}</p>`
 				};
 			};
-			if(count === 0){
-				lives = lives - 1;
-				document.getElementById("guessRemain").innerHTML = `<p>${lives}</p>`;
-			}  else if(currentBlank.indexOf("-") === -1){					
-					alert("YOU WIN YOU GENIUS!");
-					currentScore = currentScore + 1;
-					document.getElementById("score").innerHTML = `<p>${currentScore}</p>`;
-					document.getElementById("icons").innerHTML = `<img src = "assets/images/${displayIcon}">`;
-					gameStatus = 0;	
-			};
-		} else {
-			alert("Better luck next time!");
-			gameStatus = 0;
+// 			if(count === 0){
+// 				lives = lives - 1;
+// 				document.getElementById("guessRemain").innerHTML = `<p>${lives}</p>`;
+// 			}  else if(currentBlank.indexOf("-") === -1){					
+// 					alert("YOU WIN YOU GENIUS!");
+// 					currentScore = currentScore + 1;
+// 					document.getElementById("score").innerHTML = `<p>${currentScore}</p>`;
+// 					document.getElementById("icons").innerHTML = `<img src = "assets/images/${displayIcon}">`;
+// 					gameStatus = 0;	
+// 			};
+// 		} else {
+// 			alert("Better luck next time!");
+// 			gameStatus = 0;
+// 		};
 		};
-	};
-}	
-	
+ 	};	
+};
 
 
 
