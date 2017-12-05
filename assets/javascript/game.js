e5c532c5a7aa687b225a83753dc334ad6d0ba95e
Quan Gao <quangao@Quans-MacBook-Pro.local>
2017-12-04 23:20:16 -0600

    add description to JS file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,7 +8,6 @@ var gameStatus = 0;
 var currentScore = 0;
 var letters = [];
 var lives = 10;
-var currentBlank = [];
 var currentWord = "";
 
 // This function replace letters in a string (or an array) into an array of dashes while keeping the space between 2 or more words
@@ -35,7 +34,8 @@ function word(name, icon) {
 };
 
 document.onkeyup = function(event) {
-	var playerChoice = event.key;		
+	var playerChoice = event.key;
+	// set all paramters at the begining of the game (gameStatus === 0)	and push the values to the html file.
 	if (gameStatus === 0) {
 		select = Math.floor(Math.random()*words.length);
 		currentWord = new word(words[select],icons[select]);
@@ -47,32 +47,36 @@ document.onkeyup = function(event) {
 		letters = [];	
 		document.getElementById("progress").innerHTML = `<p>${letters}</p>`;
 		gameStatus = 1;	
-
+	// After game start (gameStatus === 1), only react when the letter of input (event.key) hasn't been pressed before  	
 	} else if(gameStatus === 1 && letters.indexOf(playerChoice) === -1) {
 		letters.push(playerChoice);
 		document.getElementById("progress").innerHTML = `<p>${letters}</p>`;
+	// if there are guesses remain and guess corrently, replace dash with letter. if guess incorrectly, reduce guesses remain by 1
 		if(lives > 0) {
 			var count = 0;
-			currentBlank = currentWord.dashArr;
-			for (i = 0; i < currentBlank.length; i ++) {
+			for (i = 0; i < currentWord.length; i ++) {
 				if((currentWord.nameArr)[i] === playerChoice) {
-					currentBlank[i] = playerChoice;
+					currentWord.dashArr[i] = playerChoice;
 					count++;
 				};
-			document.getElementById("display").innerHTML = `<p>${currentBlank.join("")}</p>`
+			document.getElementById("display").innerHTML = `<p>${currentWord.dashArr.join("")}</p>`
 			};
 			if(count === 0){
 				lives = lives - 1;
 				document.getElementById("guessRemain").innerHTML = `<p>${lives}</p>`;
-			}  else if(currentBlank.indexOf("-") === -1){					
+		// if all dashes have been replaced, you win and add one to "win"
+			}  else if((currentWord.dashArr).indexOf("-") === -1){						
 					alert("YOU WIN YOU GENIUS!");
 					currentScore = currentScore + 1;
 					document.getElementById("score").innerHTML = `<p>${currentScore}</p>`;
 					document.getElementById("icons").innerHTML = `<img src = ${currentWord.icon}>`;
 					gameStatus = 0;	
 			};
+	// if you use all guesses and didn't finish the word, you lose.
 		} else {
 			alert("Better luck next time!");
+			document.getElementById("display").innerHTML = `<p>${currentWord.name}</p>`;
+			document.getElementById("icons").innerHTML = `<img src = ${currentWord.icon}>`;
 			gameStatus = 0;
 		};
 
